56f2a460752745e1e93d7783137fe351aec7784f
lucas_passos <lsiqueira355@gmail.com>
2020-10-09 01:44:53 -0300

    Updates

---
+++ precache-manifest.a055ce17b9ef9971868c8ee4e07beba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314dd9e1608597926d6479c2c10e26e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d16f18322418dd3b278d",
+    "url": "/static/css/2.2e43db08.chunk.css"
+  },
+  {
+    "revision": "278b15049441b78657ed",
+    "url": "/static/css/main.64725961.chunk.css"
+  },
+  {
+    "revision": "d16f18322418dd3b278d",
+    "url": "/static/js/2.e9c64634.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e9c64634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278b15049441b78657ed",
+    "url": "/static/js/main.774145c4.chunk.js"
+  },
+  {
+    "revision": "025c5ad1b590432f5711",
+    "url": "/static/js/runtime-main.b8a5d5fe.js"
+  }
+]);
